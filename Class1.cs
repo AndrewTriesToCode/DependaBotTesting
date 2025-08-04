78ca202467d3ed805f004f736fd55dd2039a9168
Andrew White <andrew@finbuckle.com>
2025-08-04 14:22:30 -0400

    added vuln system.text.json to see what dependabot will do

--- Class1.cs
+++ Class1.cs
@@ -2,5 +2,9 @@
 
 public class Class1
 {
-
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; init; }
 }
